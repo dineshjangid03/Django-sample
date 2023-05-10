7774f31d4259e226cb3334dba6bc56e185296679
dineshjangid03 <dinesh.jangid323@gmail.com>
2023-05-10 13:24:08 +0530

    sample django project

---
+++ Hello/home/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("this is aboutpage")
+
+def service(request):
+    return render(request, 'service.html')
+    # return HttpResponse("this is servicepage")
+
+def contact(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        contact=Contact(name=name, email=email, phone=phone)
+        contact.save()
+    
+    return render(request, 'contact.html')
+    # return HttpResponse("this is contactpage")
